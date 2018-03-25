77440bf2b667e3583899e2456d7f66235a3adc04
timothygk <timothykurniawan@gmail.com>
2018-03-25 14:34:37 +0800

    update util

--- src/tools/util.py
+++ src/tools/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import keras.backend as K
+from PIL import Image
 from keras.callbacks import Callback
 from keras.models import Model
 from keras.layers import Dense, Flatten, Dropout, AveragePooling2D
@@ -11,42 +12,52 @@ from keras.applications import (
     inception_v3,
     resnet50,
     inception_resnet_v2,
-    xception
+    xception,
+    densenet
 )
-
 
 def load_preprocess_input(model_type):
 
     if model_type == "VGG19":
         preprocess_input = vgg19.preprocess_input
+        input_shape = (224, 224)
     elif model_type == "VGG16":
         preprocess_input = vgg16.preprocess_input
+        input_shape = (224, 224)
     elif model_type == "InceptionResNetV2":
         preprocess_input = inception_resnet_v2.preprocess_input
+        input_shape = (299, 299)
     elif model_type == "ResNet50":
         preprocess_input = resnet50.preprocess_input
+        input_shape = (224, 224)
     elif model_type == "Xception":
         preprocess_input = xception.preprocess_input
+        input_shape = (299, 299)
+    elif "DenseNet" in model_type:
+        preprocess_input = densenet.preprocess_input
+        input_shape = (224, 224)
     else:
         preprocess_input = inception_v3.preprocess_input
+        input_shape = (299, 299)
 
-    return preprocess_input
+    return preprocess_input, input_shape
 
 
-def load_model(model_type, input_shape, n_classes=None, include_top=True, stack_new_layers=True, flatten_fn=Flatten(), dropout_rate=0.5):
-
+def load_model(model_type, input_shape, pooling=None, n_classes=None, include_top=True, stack_new_layers=True, flatten_fn=Flatten(), dropout_rate=0.5):
+    # TODO: add params --> pooling, include_top
+    # TODO: input shape
     if model_type == "VGG19":
-        base_model = vgg19.VGG19(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = vgg19.VGG19(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
     elif model_type == "VGG16":
-        base_model = vgg16.VGG16(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = vgg16.VGG16(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
     elif model_type == "InceptionResNetV2":
-        base_model = inception_resnet_v2.InceptionResnetV2(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = inception_resnet_v2.InceptionResnetV2(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
     elif model_type == "ResNet50":
-        base_model = resnet50.ResNet50(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = resnet50.ResNet50(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
     elif model_type == "Xception":
-        base_model = xception.Xception(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = xception.Xception(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
     else:
-        base_model = inception_v3.InceptionV3(weights="imagenet", include_top=include_top, input_shape=input_shape)
+        base_model = inception_v3.InceptionV3(weights="imagenet", include_top=include_top, input_shape=input_shape, pooling=pooling)
 
     if include_top:
 
@@ -78,16 +89,27 @@ def load_images(filepaths, input_shape):
 
     all_imgs, broken_imgs = [], []
 
-    for i, filename in enumerate(filepaths):
+    for i, (j, filename) in enumerate(filepaths.iteritems()):
+        if i % 1000 == 0:
+            print('load', i)
         try:
             img = load_img(filename, target_size=input_shape)
             img = img_to_array(img).astype(np.float32)
             all_imgs.append(img)
         except:
-            broken_imgs.append(i)
-            print(filename, "is broken")
+            broken_imgs.append(j)
+            # print(filename, "is broken")
 
+    print('check', len(all_imgs), len(broken_imgs))
     return all_imgs, broken_imgs
+
+
+def resize_image(img, thumbnail_size, to_array=True):
+    tmp = Image.fromarray(img.astype(np.uint8))
+    tmp = tmp.resize(thumbnail_size)
+    if to_array:
+        tmp = np.array(tmp).astype(np.float32)
+    return tmp
 
 
 class LearningRateTracker(Callback):
