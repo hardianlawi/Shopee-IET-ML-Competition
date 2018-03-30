bd6bc7e8be4cc91d9394d610421225f21fb4d2bc
hardianlawi <hlawi001@e.ntu.edu.sg>
2018-03-30 13:31:33 +0800

    Updated scripts

--- src/training.py
+++ src/training.py
@@ -9,20 +9,11 @@ from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger, EarlyStopping, LearningRateScheduler
 
-from tools.util import load_model, load_images, load_preprocess_input, scheduler, LearningRateTracker, ModelMGPU
+from tools.util import load_model, load_images, load_preprocess_input, scheduler, LearningRateTracker, ModelMGPU, load_default_input_shape
 
 
 np.random.seed(2018)
 K.clear_session()
-
-# Model to use
-model_type = "NASNetLarge"
-include_top = True
-stack_new_layers = True
-input_shape = (331, 331)
-dropout_rate = 0.5
-n_classes = 18
-no_of_gpus = 1
 
 # Filepaths
 output_dir = "../outputs"
@@ -60,41 +51,56 @@ model_dir = os.path.join(output_dir, 'saved_models')
 if not os.path.isdir(model_dir) or not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
-# Training Config
-n_splits = 7  # No of split for skfold cross validation
-batch_size = 32  # No of samples fit every step
-epochs = 50  # No of epochs
-lr = 0.005  # Optimizer learning rate
 
-# Read pre-generated dataset comprising of 3 columns (file, species, species_id)
-tDf = pd.read_csv(testDataset)
+def main(model_type):
 
-# Load test images
-Xtest, broken_test_imgs = load_images(tDf.file, input_shape=input_shape)
+    # Load default input shape and preprocess_input
+    input_shape = load_default_input_shape(model_type)
+    preprocess_input = load_preprocess_input(model_type)
 
-# Load preprocessor based on model_type
-preprocess_input = load_preprocess_input(model_type)
+    # Configs
+    include_top = True
+    stack_new_layers = True
+    dropout_rate = 0.5
+    n_classes = 18
+    no_of_gpus = 1
 
-# Convert and process test images
-Xtest = np.asarray(Xtest)
-Xtest = preprocess_input(Xtest)
+    # Training Config
+    n_splits = 7  # No of split for skfold cross validation
+    batch_size = 32  # No of samples fit every step
+    epochs = 50  # No of epochs
+    lr = 0.005  # Optimizer learning rate
 
-# Check shapes
-assert Xtest.shape == (tDf.shape[0] - len(broken_test_imgs),) + input_shape + (3, )
+    # Read pre-generated dataset comprising of 3 columns (file, species, species_id)
+    tDf = pd.read_csv(testDataset)
 
-# Training
-print("Start cross-validation training...")
-histories = []
-valDf = pd.DataFrame()
-for iteration in range(n_splits):
+    # Load test images
+    Xtest, broken_test_imgs = load_images(tDf.file, input_shape=input_shape)
 
-    # Define model name
-    model_name = '%s_model_{epoch:03d}_{val_acc:.2f}_iter%d.h5' % (model_type, iteration)
-    filepath = os.path.join(model_dir, model_name)
+    # Training
+    print("Start cross-validation training...")
+    histories = []
+    for iteration in range(n_splits):
 
-    # Load model
-    if no_of_gpus > 1:
-        with tf.device("/cpu:0"):
+        # Define model name
+        model_name = '%s_model_{epoch:03d}_{val_acc:.2f}_iter%d.h5' % (model_type, iteration)
+        filepath = os.path.join(model_dir, model_name)
+
+        # Load model
+        if no_of_gpus > 1:
+
+            with tf.device("/cpu:0"):
+                model = load_model(
+                    model_type,
+                    input_shape=input_shape + (3,),
+                    n_classes=n_classes,
+                    include_top=include_top,
+                    stack_new_layers=stack_new_layers,
+                    dropout_rate=dropout_rate
+                )
+            model = ModelMGPU(model, no_of_gpus)
+
+        else:
             model = load_model(
                 model_type,
                 input_shape=input_shape + (3,),
@@ -103,143 +109,97 @@ for iteration in range(n_splits):
                 stack_new_layers=stack_new_layers,
                 dropout_rate=dropout_rate
             )
-        model = ModelMGPU(model, no_of_gpus)
-    else:
-        model = load_model(
-            model_type,
-            input_shape=input_shape + (3,),
-            n_classes=n_classes,
-            include_top=include_top,
-            stack_new_layers=stack_new_layers,
-            dropout_rate=dropout_rate
+
+        # Define optimizer
+        sgd = optimizers.SGD(lr=lr, decay=1e-6, momentum=0.9)
+
+        # compile the model
+        model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=["accuracy"])
+
+        # Prepare callbacks
+        checkpoint = ModelCheckpoint(
+            filepath=filepath,
+            monitor='val_acc',
+            verbose=1,
+            save_best_only=True
         )
 
-    # Define optimizer
-    sgd = optimizers.SGD(lr=lr, decay=1e-6, momentum=0.9)
+        lr_reducer = ReduceLROnPlateau(
+            monitor="val_acc",
+            factor=np.sqrt(0.1),
+            cooldown=0,
+            patience=1,
+            min_lr=0.5e-6
+        )
 
-    # compile the model
-    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=["accuracy"])
+        csv_logger = CSVLogger(os.path.join(logs_dir, "%s_training_iter%d.csv" % (model_type, iteration)))
 
-    # Prepare callbacks
-    checkpoint = ModelCheckpoint(
-        filepath=filepath,
-        monitor='val_acc',
-        verbose=1,
-        save_best_only=True
-    )
+        early_stopping = EarlyStopping(
+            monitor='val_acc',
+            min_delta=0,
+            patience=4,
+            verbose=0,
+            mode='auto'
+        )
 
-    lr_reducer = ReduceLROnPlateau(
-        monitor="val_acc",
-        factor=np.sqrt(0.1),
-        cooldown=0,
-        patience=1,
-        min_lr=0.5e-6
-    )
+        lr_scheduler = LearningRateScheduler(scheduler)
 
-    csv_logger = CSVLogger(os.path.join(logs_dir, "%s_training_iter%d.csv" % (model_type, iteration)))
+        lr_tracker = LearningRateTracker()
 
-    early_stopping = EarlyStopping(
-        monitor='val_acc',
-        min_delta=0,
-        patience=4,
-        verbose=0,
-        mode='auto'
-    )
+        callbacks = [checkpoint, lr_reducer, csv_logger, early_stopping, lr_scheduler, lr_tracker]
 
-    lr_scheduler = LearningRateScheduler(scheduler)
+        datagen = ImageDataGenerator(
+            featurewise_center=False,
+            samplewise_center=False,
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False,
+            zca_epsilon=1e-6,
+            rotation_range=0,
+            width_shift_range=0.,
+            height_shift_range=0.,
+            shear_range=0.,
+            zoom_range=0.,
+            channel_shift_range=0.,
+            fill_mode='nearest',
+            cval=0.,
+            horizontal_flip=True,
+            vertical_flip=False,
+            rescale=None,
+            preprocessing_function=preprocess_input,
+            data_format=K.image_data_format()
+        )
 
-    lr_tracker = LearningRateTracker()
+        train_generator = datagen.flow_from_directory(
+            directory=os.path.join(train_val_dir, "train_val_%d" % iteration, "train"),
+            target_size=input_shape,
+            class_mode="categorical",
+            batch_size=batch_size,
+            seed=2018
+        )
 
-    callbacks = [checkpoint, lr_reducer, csv_logger, early_stopping, lr_scheduler, lr_tracker]
+        train_val_generator = datagen.flow_from_directory(
+            directory=os.path.join(train_val_dir, "train_val_%d" % iteration, "val"),
+            target_size=input_shape,
+            class_mode="categorical",
+            batch_size=batch_size,
+            seed=2018
+        )
 
-    datagen = ImageDataGenerator(
-        featurewise_center=False,
-        samplewise_center=False,
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False,
-        zca_epsilon=1e-6,
-        rotation_range=0,
-        width_shift_range=0.,
-        height_shift_range=0.,
-        shear_range=0.,
-        zoom_range=0.,
-        channel_shift_range=0.,
-        fill_mode='nearest',
-        cval=0.,
-        horizontal_flip=True,
-        vertical_flip=False,
-        rescale=None,
-        preprocessing_function=preprocess_input,
-        data_format=K.image_data_format()
-    )
+        history = model.fit_generator(
+            generator=train_generator,
+            validation_data=train_val_generator,
+            epochs=epochs,
+            verbose=1,
+            workers=8,
+            callbacks=callbacks,
+        )
 
-    train_generator = datagen.flow_from_directory(
-        directory=os.path.join(train_val_dir, "train_val_%d" % iteration, "train"),
-        target_size=input_shape,
-        class_mode="categorical",
-        batch_size=batch_size,
-        seed=2018
-    )
+        histories.append(history)
 
-    train_val_generator = datagen.flow_from_directory(
-        directory=os.path.join(train_val_dir, "train_val_%d" % iteration, "val"),
-        target_size=input_shape,
-        class_mode="categorical",
-        batch_size=batch_size,
-        seed=2018
-    )
+        K.clear_session()
 
-    history = model.fit_generator(
-        generator=train_generator,
-        validation_data=train_val_generator,
-        epochs=epochs,
-        verbose=1,
-        workers=8,
-        callbacks=callbacks,
-    )
 
-    val_generator = datagen.flow_from_directory(
-        directory=os.path.join(train_val_dir, "train_val_%d" % iteration, "val"),
-        target_size=input_shape,
-        class_mode="categorical",
-        batch_size=100000,
-        seed=2018
-    )
-
-    # Generate second-level data
-    print("Generating second-level data...")
-    for Xval, label in val_generator:
-        val_predictions = model.predict(Xval, verbose=1)
-        valDf = pd.concat([
-            valDf,
-            pd.DataFrame(
-                np.hstack([
-                    val_predictions,
-                    np.argmax(label, axis=-1)[:, np.newaxis]
-                ]),
-                columns=["f"+str(x) for x in range(n_classes)] + ["category_id"])
-        ])
-        break
-
-    del Xval, label, val_predictions
-    gc.collect()
-
-    # Generate prediction on test data for ensembling
-    print("Generating prediction on test data...")
-    test_predictions = model.predict(Xtest, verbose=1)
-    testDf = pd.DataFrame({"id": tDf["id"]})
-    testDf = pd.concat([testDf, pd.DataFrame(test_predictions, columns=[model_type+"_f"+str(x) for x in range(n_classes)])], axis=1)
-    testDf.to_csv(
-        os.path.join(test_dir, "%s_test_iter%d.csv" % (model_type, iteration)),
-        index=False
-    )
-
-    histories.append(history)
-
-    del test_predictions, testDf
-    gc.collect()
-
-    K.clear_session()
-
-valDf.to_csv(os.path.join(val_dir, "%s_val.csv" % model_type), index=False)
+if __name__ == "__main__":
+    for model_type in ["Xception", "InceptionV3", "InceptionResNetV2", "ResNet50", "DenseNet121", "DenseNet169", "DenseNet201", "NASNetLarge", "NASNetMobile"]:
+        main(model_type)
